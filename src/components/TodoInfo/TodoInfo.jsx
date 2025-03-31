b2ee13923423a0437143fa61e0f7492941763e15
Nataliia Vdovyna <natawhite333@gmail.com>
2025-03-31 16:56:16 +0300

    improved

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,7 +1,7 @@
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
-  <article className="TodoInfo TodoInfo--completed">
+  <article className={`TodoInfo ${todo.completed && `TodoInfo--completed`}`}>
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
     <UserInfo user={todo.user} />
